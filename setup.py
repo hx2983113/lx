b3012aaa67e394caeda9a6bae03f6fdbbea0d217
lx <lx>
2020-04-30 00:00:10 +0200

    Add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'lx',         # How you named your package folder (MyLib)
   packages = ['lx'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.11',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'lx',   # Give a short description about your library
   author = 'lx',                   # Type in your name
@@ -10,7 +10,8 @@ setup(
   url = 'https://github.com/hx2983113/lx',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/hx2983113/lx/archive/0.1.tar.gz',    # I explain this later on
   keywords = [],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[
+      'numpy', 'natsort', 'opencv-python', 'PIL', 'urllib3'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
